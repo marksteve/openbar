055c4353e328795e59bae91011319323a6d886b6
Mark Steve Samson <hello@marksteve.com>
2015-01-25 00:06:50 +0800

    Add message url oembeds

--- src/jsx/chat.jsx
+++ src/jsx/chat.jsx
@@ -1,22 +1,60 @@
 var React = require('react/addons');
 var moment = require('moment');
 var marked = require('marked');
+var superagent = require('superagent');
 
-function id() {
-  return (Math.random() * Math.pow(2, 64)).toString(36);
-}
+marked.setOptions({
+  gfm: true,
+  sanitize: true
+});
+
+var URLRegex = /(http(?:s?):[^ <\n]+)/;
+var TagRegex = /<.+?>/g;
 
 var Message = React.createClass({
+  getInitialState: function() {
+    return {
+      oembed: null
+    };
+  },
+  componentDidMount: function() {
+    var urls = this.props.text.match(URLRegex);
+    if (urls) {
+      superagent
+        .get("http://iframe.ly/api/oembed")
+        .query({
+          url: urls[0],
+          api_key: "90359a4cb5007d8ac4481a"
+        })
+        .end(this._loadOembed);
+    }
+  },
+  _loadOembed: function(resp) {
+    this.setState({
+      oembed: resp.body.html
+    });
+    this.props.scrollChatToBottom();
+  },
+  _renderOembed: function() {
+    return this.state.oembed ? (
+      <div
+        className="oembed"
+        dangerouslySetInnerHTML={{__html: this.state.oembed}}
+      ></div>
+    ) : null;
+  },
   render: function() {
     var message = this.props;
+    var html = marked(message.text);
     return (
       <li>
         <span className="name">{message.name}</span>
         <span className="timestamp">{message.timestamp}</span>
         <div
           className="text"
-          dangerouslySetInnerHTML={{__html: marked(message.text)}}
+          dangerouslySetInnerHTML={{__html: html}}
         />
+        {this._renderOembed()}
       </li>
     );
   }
@@ -51,8 +89,16 @@ var Chat = React.createClass({
     var messagesDiv = this.refs.messages.getDOMNode();
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
   },
+  _renderMessage: function(message) {
+    return (
+      <Message
+        scrollChatToBottom={this.scrollToBottom}
+        {...message}
+      />
+    );
+  },
   _renderMessages: function() {
-    return this.props.messages.map(React.createFactory(Message));
+    return this.props.messages.map(this._renderMessage);
   },
   _submitMessage: function(text) {
     if (text.length < 1) {

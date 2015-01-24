f17de0b36134a7e29c11f7042d545fe154102a98
Marte Soliza <myrtactle@gmail.com>
2015-01-24 15:01:23 +0800

    Initial version of the backend.

---
+++ src/jsx/backend.js
@@ -0,0 +1,41 @@
+require('firebase');
+
+var rootRef = new Firebase('https://space-bar.firebaseio.com/');
+var barsRef = rootRef.child("bars");
+
+function Bar(ref, meta) {
+  this.ref = ref;
+  this.title = meta.title;
+}
+
+Bar.prototype.onMessage = function(callback) {
+  this.ref.child("messages").on("child_added", function(ss) {
+    callback(ss.val());
+  });
+}
+Bar.prototype.newMessage = function(message) {
+  this.ref.child("messages").push(message);
+}
+
+Bar.create = function(title) {
+  var meta = {title: title};
+  var ref = barsRef.push({meta: meta});
+  return new Bar(ref, meta);
+}
+Bar.get = function(key, callback, errCallback) {
+  var ref = barsRef.child(key);
+  ref.child("meta").once("value", function(ss) {
+    if (ss.exists()) {
+      callback(new Bar(ref, ss.val()));
+    } else {
+      errCallback(new Error("Bar does not exist."));
+    }
+  }, errCallback);
+}
+
+
+module.exports = {
+  Bar: Bar
+}
+// for debugging						 
+window.root = rootRef;
--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -4,6 +4,9 @@ var Router = require('react-router');
 require("react-tap-event-plugin")();
 window.React = React;
 
+var Backend = require('./backend.js');
+window.Backend = Backend;
+
 var Routes = require('./routes.jsx');
 
 Router
